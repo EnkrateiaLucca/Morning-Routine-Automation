be8703059c5811eb345c2f0f27b93103d704b856
Tawishi <tawishisharma1@gmail.com>
2022-01-14 18:55:58 +0530

    error checks

--- startday.py
+++ startday.py
@@ -6,6 +6,7 @@ from dotenv import load_dotenv
 
 flag = load_dotenv()
 
+alt = ""
 if not flag:
     alt = "https://www.google.com/"
 
@@ -15,61 +16,65 @@ Utility functions
 def continueDay():
     cont = input("Press ENTER too continue")
 
+def printMessage(message=""):
+    print(message)
+    time.sleep(1)
+
 '''
 Start here
 '''
 os.system("bash logSystemOnOff.sh") 
 
 # sendNotificationOnWindows("How was the workout? Share with Penzu")
-print("How was the workout? Share with Penzu")
+printMessage("How was the workout? Share with Penzu")
 webbrowser.open(os.getenv("PENZU",alt))
 continueDay()
 
 # notion
-print("Check your calendar")
-print("Update Daily Tasks")
+printMessage("Check your calendar")
+printMessage("Update Daily Tasks")
 webbrowser.open_new_tab(os.getenv("NOTION",alt))
 continueDay()
 
 # start touch type measure and practice
-print("test 1")
+printMessage("test 1")
 webbrowser.open(os.getenv("TEST1",alt))
 continueDay()
-print("test 2")
+printMessage("test 2")
 webbrowser.open(os.getenv("TEST2",alt))
 continueDay()
-print("test 3")
+printMessage("test 3")
 webbrowser.open(os.getenv("TEST3",alt))
 continueDay()
-print("test 4")
+printMessage("test 4")
 webbrowser.open(os.getenv("TEST4",alt))
 continueDay()
 
 # cal & update daily type progess
-print("WPM daily update")
+printMessage("WPM daily update")
 # subprocess.Popen('C:\\Windows\\System32\\calc.exe')
 webbrowser.open(os.getenv("T_WORKSHEET",alt))
 continueDay()
 
 # anki
-print("Anki time!")
+printMessage("Anki time!")
 webbrowser.open(os.getenv("ANKI"),alt)
 continueDay()
 
 # mail
-print("Check mail")
+printMessage("Check mail")
 webbrowser.open(os.getenv("MAIL",alt))
 continueDay()
 
 # whatsapp
-print("Check whatsapp")
+printMessage("Check whatsapp")
 # sendNotificationOnWindows("Open Whatsapp")
 webbrowser.open(os.getenv("WHATSAPP",alt))
 continueDay()
 
 # leetcode
-print("DSA Practice")
+printMessage("DSA Practice")
 webbrowser.open(os.getenv("LEETCODE",alt))
 continueDay()
 
-print("Let's take a bread!")
+printMessage("Let's take a bread!")
