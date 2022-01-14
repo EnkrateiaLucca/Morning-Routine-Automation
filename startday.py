d9a24f4cbfb414e37f8f5be93d9007469d9acb79
Tawishi <tawishisharma1@gmail.com>
2022-01-14 18:28:37 +0530

    set variables

--- startday.py
+++ startday.py
@@ -31,65 +31,62 @@ def sendNotificationOnWindows(title, message, app_image=None):
 
 def continueDay():
     cont = input("Press ENTER too continue")
-    # sendNotificationOnWindows("continue","CONTINUE?")
 
 '''
 Start here
 '''
 os.system("bash logSystemOnOff.sh") 
 
-# # sendNotificationOnWindows("How was the workout? Share with Penzu")
-# print("How was the workout? Share with Penzu")
-# webbrowser.open("https://penzu.com/journals/25285684/new")
-# continueDay()
+# sendNotificationOnWindows("How was the workout? Share with Penzu")
+print("How was the workout? Share with Penzu")
+webbrowser.open(os.getenv(PENZU))
+continueDay()
 
-# # notion
-# print("Check your calendar")
-# print("Update Daily Tasks")
-# webbrowser.open_new_tab("www.notion.so")
-# continueDay()
+# notion
+print("Check your calendar")
+print("Update Daily Tasks")
+webbrowser.open_new_tab(os.getenv(NOTION))
+continueDay()
 
-# # start touch type measure and practice
-# print("test 1")
-# webbrowser.open("https://10fastfingers.com/typing-test/english")
-# continueDay()
-# print("test 2")
-# webbrowser.open("https://10fastfingers.com/top1000")
-# continueDay()
-# print("test 3")
-# webbrowser.open("https://typetest.io/")
-# continueDay()
-# print("test 4")
-# webbrowser.open("https://www.keybr.com/")
-# continueDay()
+# start touch type measure and practice
+print("test 1")
+webbrowser.open(os.getenv(TEST1))
+continueDay()
+print("test 2")
+webbrowser.open(os.getenv(TEST2))
+continueDay()
+print("test 3")
+webbrowser.open(os.getenv(TEST3))
+continueDay()
+print("test 4")
+webbrowser.open(os.getenv(TEST4))
+continueDay()
 
 # cal & update daily type progess
 print("WPM daily update")
 # subprocess.Popen('C:\\Windows\\System32\\calc.exe')
-webbrowser.open("https://docs.google.com/spreadsheets/d/1q3SI5mlltNWSbDY8oEg-ID4EAzzs0jBEX7uA-BCHkNQ/edit#gid=0")
+webbrowser.open(os.getenv(T_WORKSHEET))
 continueDay()
 
 # anki
 print("Anki time!")
-webbrowser.open("https://ankiweb.net/decks/")
+webbrowser.open(os.getenv(ANKI))
 continueDay()
 
 # mail
 print("Check mail")
-webbrowser.open("https://mail.google.com/mail/u/0/#inbox")
+webbrowser.open(os.getenv(MAIL))
 continueDay()
 
 # whatsapp
 print("Check whatsapp")
 # sendNotificationOnWindows("Open Whatsapp")
-webbrowser.open("https://web.whatsapp.com/")
+webbrowser.open(os.getenv(WHATSAPP))
 continueDay()
 
 # leetcode
 print("DSA Practice")
-webbrowser.open("https://leetcode.com/problem-list/3d1b6ab26e9d4b5bbc663aaa1010f252/?sorting=W3sic29ydE9yZGVyIjoiQVNDRU5ESU5HIiwib3JkZXJCeSI6IkRJRkZJQ1VMVFkifV0%3D&page=1")
+webbrowser.open(os.getenv(LEETCODE))
 continueDay()
 
 print("Let's take a bread!")
-
-
