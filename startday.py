5e4ea7d0cd166d060c2a9d32b85c1de9a5769a9a
Tawishi <tawishisharma1@gmail.com>
2022-01-14 18:42:47 +0530

    error resolve

--- startday.py
+++ startday.py
@@ -2,7 +2,12 @@ import webbrowser
 import time
 import os
 from datetime import datetime
+from dotenv import load_dotenv
 
+flag = load_dotenv()
+
+if not flag:
+    alt = "https://www.google.com/"
 
 '''
 Utility functions
@@ -17,54 +22,54 @@ os.system("bash logSystemOnOff.sh")
 
 # sendNotificationOnWindows("How was the workout? Share with Penzu")
 print("How was the workout? Share with Penzu")
-webbrowser.open(os.getenv(PENZU))
+webbrowser.open(os.getenv("PENZU",alt))
 continueDay()
 
 # notion
 print("Check your calendar")
 print("Update Daily Tasks")
-webbrowser.open_new_tab(os.getenv(NOTION))
+webbrowser.open_new_tab(os.getenv("NOTION",alt))
 continueDay()
 
 # start touch type measure and practice
 print("test 1")
-webbrowser.open(os.getenv(TEST1))
+webbrowser.open(os.getenv("TEST1",alt))
 continueDay()
 print("test 2")
-webbrowser.open(os.getenv(TEST2))
+webbrowser.open(os.getenv("TEST2",alt))
 continueDay()
 print("test 3")
-webbrowser.open(os.getenv(TEST3))
+webbrowser.open(os.getenv("TEST3",alt))
 continueDay()
 print("test 4")
-webbrowser.open(os.getenv(TEST4))
+webbrowser.open(os.getenv("TEST4",alt))
 continueDay()
 
 # cal & update daily type progess
 print("WPM daily update")
 # subprocess.Popen('C:\\Windows\\System32\\calc.exe')
-webbrowser.open(os.getenv(T_WORKSHEET))
+webbrowser.open(os.getenv("T_WORKSHEET",alt))
 continueDay()
 
 # anki
 print("Anki time!")
-webbrowser.open(os.getenv(ANKI))
+webbrowser.open(os.getenv("ANKI"),alt)
 continueDay()
 
 # mail
 print("Check mail")
-webbrowser.open(os.getenv(MAIL))
+webbrowser.open(os.getenv("MAIL",alt))
 continueDay()
 
 # whatsapp
 print("Check whatsapp")
 # sendNotificationOnWindows("Open Whatsapp")
-webbrowser.open(os.getenv(WHATSAPP))
+webbrowser.open(os.getenv("WHATSAPP",alt))
 continueDay()
 
 # leetcode
 print("DSA Practice")
-webbrowser.open(os.getenv(LEETCODE))
+webbrowser.open(os.getenv("LEETCODE",alt))
 continueDay()
 
 print("Let's take a bread!")
